046fd9741d3a5b4193c951e1f7e0a6b2dc533a6d
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2025-01-30 02:57:21 +0530

    add to git

--- Patterns.java
+++ Patterns.java
@@ -1,7 +1,19 @@
 
 public class Patterns {
     public static void main(String[] args) {
-        // char a = '*';
+        // // Question01
+        // // *****
+        // // *****
+        // // *****
+        // // *****
+        // // *****
+        // for (int i = 0; i < 5; i++) {
+        //     for (int j = 0; j < 5; j++) {
+        //         System.out.print('*');
+        //     }
+        //     System.out.println( );
+        // }
+        // // Question 02
         // // *
         // // **
         // // ***
@@ -9,26 +21,125 @@ public class Patterns {
         // // *****
         // for (int i = 0; i < 5; i++) {
         //     for (int j = 0; j < i+1; j++) {
-        //         System.out.print(a);
+        //         System.out.print('*');
         //     }
         //     System.out.println( );
         // }
+        // // Question 03
         // // *****
         // // ****
         // // ***
         // // **
         // // *
         // for (int i = 0; i < 5; i++) {
+        //      for (int j = i; j < 5; j++) {
+        //          System.out.print('*');
+        //      }
+        //      System.out.println( );
+        //  }
+        // for (int i = 0; i < 5; i++) {
+            // for (int j = 0; j < 5 - i; j++) {
+                // System.out.print('*');
+            // }
+            // System.out.println( );
+        // }
+        // // Question 04
+        // // 1
+        // // 12
+        // // 123
+        // // 1234
+        // // 123345
+        // int num = 1;
+        // for (int i = 0; i < 5; i++) {
+        //     for (int j = 0; j < i+1; j++) {
+        //         System.out.print(num );
+        //     } 
+        //     System.out.println( );
+        // }
+        // // Question 05
+        // // 1
+        // // 22
+        // // 333
+        // // 4444
+        // // 55555
+        // int num = 1;
+        // for (int i = 0; i < 5; i++) {
+        //     for (int j = 0; j < i+1; j++) {
+        //         System.out.print(num );
+        //     }
+        //     num++;  
+        //     System.out.println( );
+        // }
+        // // Question 06
+        // // 12345
+        // // 1234
+        // // 123
+        // // 12
+        // // 1
+        // int num = 1;
+        // for (int i = 0; i < 5; i++) {
         //     for (int j = i; j < 5; j++) {
-        //         System.out.print(a);
+        //         System.out.print(num + j - i);
         //     }
         //     System.out.println( );
         // }
-        char c = 'A';
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < i+1; j++) {
-                System.out.print(c) ;
-                c++;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (j < 4 - i -1){
+                    System.out.print(' ');
+                } else if (j > 4 + i -1) {
+                    System.out.print(' ');
+                } else {
+                    System.out.print("*");
+                }
+                }
+            System.out.println( );
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (i > j ){
+                    System.out.print(' ');
+                } else if (j >= 7-i){
+                    System.out.print(' ');
+                } else {
+                    System.out.print('*');
+                }
+            }
+            System.out.println( );
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (j < 4 - i -1){
+                    System.out.print(' ');
+                } else if (j > 4 + i -1) {
+                    System.out.print(' ');
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println( );
+        }
+        for (int i = 4; i < 8; i++) {
+            for (int j = 4; j < 11; j++) {
+                if (i > j ){
+                    System.out.print(' ');
+                } else if (j > 14-i){
+                    System.out.print(' ');
+                } else {
+                    System.out.print('*');
+                }
+            }
+            System.out.println( );
+        }
+        for (int i = 0 ; i < 5 ; i++){
+            for(int j = 0 ; j < i+1; j ++){
+                System.out.print('*');
+            }
+            System.out.println( );
+        }
+        for (int i = 0 ; i < 5 ; i++) {
+            for(int j = 0 ; j < 4 - i; j ++){
+                System.out.print('*');
             }
             System.out.println( );
         }
--- Sample.java
+++ Sample.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.sql.SQLOutput;
 
 public class Sample {
+    public static void main(String[] args) {
+        for (int i = 0 ; i < 7 ; i++){
+            for(int j = 0 ; j < 4; j ++) {
+                if (j <= i){
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+    }
 }
