dfce8b51c33dac5d21898d31e5944b5fb3ad3fad
Pradyumn Pratap Singh <singhpradyumnpratap@gmail.com>
2025-01-29 18:48:36 +0530

    Add to git

---
+++ Operators_in_java.java
@@ -0,0 +1,37 @@
+public class Operators_in_java{
+     public static void main(String[] args) {
+         // Arithmetic Operators
+
+          // Binary (2 operands needeed)
+          //  + , - , * , / , %
+          int a = 10;
+          int b = 5;
+          System.out.println("Addtion(+) : " +  (a + b));
+          System.out.println("Subtraction(-) : " +  (a - b)) ;
+          System.out.println("Multiplication(*) : " +  a * b);
+          System.out.println("Division(/) : " +  a / b);
+          System.out.println("Modulo(%) : " +  a % b);
+
+          // Unary ( 1 operands )
+          // Increment and decrement 
+          // ++ and --
+          // post and pre 
+          // a++ and ++a
+          int c = 10;
+          int d = c++ ;
+          System.out.println(c);
+          System.out.println(d);
+
+
+
+          // Relation Operators
+
+          // Logical Operators
+
+          // Bitwise Operators
+
+          // Assignment  Operators
+
+     }
+}
+
